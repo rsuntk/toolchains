50459286c759e0e3970e2e1721f2d3a13c41b504
Rissu <90097027+rsuntk@users.noreply.github.com>
2025-01-13 20:23:28 +0700

    feat: Add clone script

---
+++ clone.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+clone() {
+	echo "[+] Cloning $@ into $@ .."
+	git clone --depth=1 --single-branch https://github.com/rsuntk/toolchains.git -b $@ $@
+	echo "[+] Clone done."
+}
+
+[ ! -z $@ ] && clone $@ || echo "[-] `basename $0` need argument!" && exit

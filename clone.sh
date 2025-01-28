4d12391d7f4e1c61734ad81835bd19ebd01d169e
Rissu <90097027+rsuntk@users.noreply.github.com>
2025-01-28 15:14:50 +0700

    clone: Checks if cloned successfully

--- clone.sh
+++ clone.sh
@@ -2,8 +2,8 @@
 
 clone() {
 	echo "[+] Cloning $@ into $@ .."
-	git clone --depth=1 --single-branch https://github.com/rsuntk/toolchains.git -b $@ $@ &> /dev/null
-	echo "[+] Clone done."
+	git clone --depth=1 --single-branch https://github.com/rsuntk/toolchains.git -b $@ $@
+	[ -d $(pwd)/$@ ] && echo "[+] Clone done." || "[-] Clone failed!" && exit
 }
 
 [ ! -z $@ ] && clone $@ || echo "[-] `basename $0` need argument!" && exit

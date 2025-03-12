635fe747e30e6cb8891ab2f96207cfa027b1c17f
Rissu <90097027+rsuntk@users.noreply.github.com>
2025-03-12 14:38:21 +0700

    clone: Silent 'Cloning into'

--- clone.sh
+++ clone.sh
@@ -2,7 +2,7 @@
 
 clone() {
 	echo "INFO: Fetching branch ($@) into $@ folder ..."
-	git clone --depth=1 --single-branch https://github.com/rsuntk/toolchains.git -b $@ $@
+	git clone --depth=1 --single-branch https://github.com/rsuntk/toolchains.git -b $@ $@ >/dev/null 2>&1
 	[ -d $(pwd)/$@ ] && echo "INFO: Successfully fetched $@." || echo "FAILED: Fetch failed, cleaning up." && exit
 }
 

7d24ea498138788ba00baaf4ba62f557e31a2e57
Rissu <90097027+rsuntk@users.noreply.github.com>
2025-03-20 02:24:39 +0700

    clone: Specify directory

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 clone() {
-	echo "INFO: Fetching branch ($@) into $@ folder ..."
+	echo "INFO: Fetching branch ($@) into [$(pwd)/$@] ..."
 	git clone --depth=1 --single-branch https://github.com/rsuntk/toolchains.git -b $@ $@ >/dev/null 2>&1
 	[ -d $(pwd)/$@ ] && echo "INFO: Successfully fetched $@." || echo "FAILED: Fetch failed, cleaning up." && exit
 }
